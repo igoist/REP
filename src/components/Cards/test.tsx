8154dcee06d76c6fad92694ba63253a4bceb004b
igoist <493512132@qq.com>
2020-04-09 16:18:47 +0800

    Solve the render problem in test & add lock.

--- src/components/Cards/test.tsx
+++ src/components/Cards/test.tsx
@@ -31,13 +31,23 @@ let arr = [
 
 import * as React from 'react';
 
-const { useState, useEffect } = React;
+const { useState, useEffect, useRef } = React;
+
+type CardPropsType = {
+  i: number,
+  c: string,
+  di: number
+};
 
 const useCardsStatus = () => {
   const [indexArr, setIndexArr] = useState([0, 1, 2, 3, 4, 5, 6]);
+  const [locked, setLocked] = useState(false);
   const [list, setList] = useState([]);
 
   const toNext = () => {
+    if (locked) {
+      return;
+    }
     let t: Array<number> = indexArr.map(i => i);
     let tmp = t.shift();
     t.push(tmp);
@@ -46,6 +56,9 @@ const useCardsStatus = () => {
   };
 
   const toPrev = () => {
+    if (locked) {
+      return;
+    }
     let t: Array<number> = indexArr.map(i => i);
     let tmp = t.pop();
     t.unshift(tmp);
@@ -53,7 +66,7 @@ const useCardsStatus = () => {
     setIndexArr(t);
   };
 
-  return { indexArr, list, setList, toPrev, toNext };
+  return { indexArr, list, setList, setLocked, toPrev, toNext };
 };
 
 // type SingleCardType = {
@@ -62,7 +75,40 @@ const useCardsStatus = () => {
 // };
 
 const Cards = () => {
-  const { indexArr, list, setList, toPrev, toNext } = useCardsStatus();
+  const [initFlag, setInitFlag] = useState(true);
+  const { indexArr, list, setList, setLocked, toPrev, toNext } = useCardsStatus();
+  let tmpNode = useRef(null);
+
+  const Card = (props: CardPropsType) => {
+    const { i, c, di } = props;
+    const tmpNode = useRef(null);
+    let tmpClassName = `ex-router-card ex-router-card-${i}`;
+    if (i === 0) {
+      tmpClassName = `ex-router-card ex-router-card-${list.length - 1} ex-router-card-leave`;
+    }
+
+    useEffect(() => {
+      if (initFlag) {
+        setInitFlag(false);
+        return;
+      }
+      if (i === 0) {
+        tmpNode.current.addEventListener('animationend', () => {
+          tmpNode.current.className = 'ex-router-card ex-router-card-0';
+        });
+
+        tmpNode.current.classList.add('ex-router-card-leave-active');
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return <div className={tmpClassName} style={{ backgroundColor: c }} data-index={di} ref={tmpNode} />;
+  };
+
+  const Card2 = (props: CardPropsType) => {
+    const { i, c, di } = props;
+    return <div key={i.toString()} className={`ex-router-card ex-router-card-${i}`} style={{ backgroundColor: c }} data-index={di} />;
+  };
 
   const JustARenderer = () => {
     return (
@@ -79,12 +125,34 @@ const Cards = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    if (initFlag) {
+      setInitFlag(false);
+      return;
+    }
+    setLocked(true);
+    tmpNode.current.addEventListener('animationend', () => {
+      tmpNode.current.className = 'ex-router-card ex-router-card-0';
+      setLocked(false);
+    });
+
+    tmpNode.current.classList.add('ex-router-card-leave-active');
+  }, [initFlag, indexArr, setLocked]);
+
   return (
     <div className='ex-iframe'>
       <div className='ex-router-cards' onClick={toNext}>
         {/* <JustARenderer /> */}
         {list.map((item, i) => {
-          return <div className={`ex-router-card ex-router-card-${indexArr[i]}`} style={{ backgroundColor: item.color }} data-index={i} />;
+          let tmpClassName = `ex-router-card ex-router-card-${indexArr[i]}`;
+          if (indexArr[i] === 0) {
+            tmpClassName = `ex-router-card ex-router-card-${list.length - 1} ex-router-card-leave`;
+            return <div key={i.toString()} className={tmpClassName} style={{ backgroundColor: item.color }} data-index={i} ref={tmpNode} />;
+          } else {
+            return <div key={i.toString()} className={tmpClassName} style={{ backgroundColor: item.color }} data-index={i} />;
+          }
+          // return <Card key={i.toString()} i={indexArr[i]} c={item.color} di={i} />;
+          // return <Card2 key={i.toString()} i={indexArr[i]} c={item.color} di={i} />;
         })}
       </div>
     </div>
