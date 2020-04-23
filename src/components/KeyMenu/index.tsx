1b3cadeab6a0bbb793cbeaac12d1c270af777e65
igoist <493512132@qq.com>
2020-04-24 00:56:56 +0800

    Deep copy for objects array

--- src/components/KeyMenu/index.tsx
+++ src/components/KeyMenu/index.tsx
@@ -1,31 +1,123 @@
 import * as React from 'react';
-import { EX } from '@Utils';
+import { EX, copy } from '@Utils';
+import { Store } from '@Contexts';
 
 const prefix = `${EX}-keyMenu`;
-const arr = ['A', 'S', 'D', 'F', 'G', 'H', 'J'];
 
-const { useEffect } = React;
+const { useEffect, useMemo } = React;
+
+type KeyMenuItemType = {
+  id: number,
+  text: string,
+  desc: string,
+  active: boolean
+};
+
+type InitialStateType = {
+  keyMenuArr: Array<KeyMenuItemType>
+};
+
+const initialKeyMenuState: InitialStateType = {
+  keyMenuArr: [
+    {
+      id: 0,
+      text: 'A',
+      desc: 'DarkMode',
+      active: false
+    },
+    {
+      id: 1,
+      text: 'S',
+      desc: 'FontColor',
+      active: false
+    },
+    {
+      id: 2,
+      text: 'D',
+      desc: 'NoBgImage',
+      active: false
+    },
+    {
+      id: 3,
+      text: 'F',
+      desc: 'NoImage',
+      active: false
+    },
+    {
+      id: 4,
+      text: 'G',
+      desc: 'ReadCode',
+      active: false
+    },
+    {
+      id: 5,
+      text: 'H',
+      desc: '........',
+      active: false
+    },
+    {
+      id: 6,
+      text: 'J',
+      desc: 'Switch',
+      active: false
+    }
+  ]
+};
 
 const KeyMenu = () => {
+  const store = Store.useContainer();
+
+  const { storeState, pushStore, toPrevStore, toNextStore } = store;
+
+  let storeCurrent = storeState.storeList[storeState.current];
+
+  let handleClick = (tmpId: number) => {
+    let tmpArr: Array<KeyMenuItemType> = storeCurrent.keyMenuArr.map(copy.deepCopyOA);
+    for (let i = 0; i < tmpArr.length; i++) {
+      if (tmpArr[i].id === tmpId) {
+        tmpArr[i].active = !tmpArr[i].active;
+        break;
+      }
+    }
+
+    pushStore({
+      ...storeCurrent,
+      keyMenuArr: tmpArr.map(i => i)
+    });
+  };
+
   useEffect(() => {
-    setTimeout(() => {
-      console.log('ssssss');
-    }, 2000);
+    pushStore({ ...initialKeyMenuState });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return (
-    <div className={`${prefix}-wrap`}>
-      <div className={`${prefix}-row`}>
-        {arr.map((item, i) => {
-          return (
-            <div className={`${prefix}-item ${prefix}-item-${i}`} key={`${prefix}-item-${item}`}>
-              <button className={`${prefix}-btn-neon ${EX}-bounce`}>{item}</button>
-            </div>
-          );
-        })}
+  const toPrev = () => {
+    console.log('enter click to Prev: ', storeState);
+    toPrevStore();
+  };
+
+  if (storeCurrent && storeCurrent.keyMenuArr) {
+    const arr = storeCurrent.keyMenuArr;
+    return (
+      <div className={`${prefix}-wrap ready`}>
+        <div className={`${prefix}-row`}>
+          {arr.map((item: KeyMenuItemType, i: number) => {
+            return (
+              <div className={`${prefix}-item ${prefix}-item-${i} ${item.active ? 'active' : ''}`} key={`${prefix}-item-${item.id}`}>
+                <button className={`${prefix}-btn-neon ${EX}-bounce`} onClick={() => handleClick(item.id)}>
+                  {item.text}
+                </button>
+              </div>
+            );
+          })}
+        </div>
+        <button onClick={toPrev}>toPrev</button>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 };
 
 export default KeyMenu;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export { default as log } from './log';
 export { default as theme } from './theme';
+export * as copy from './copy';
 export const HB = 'hb';
 export const EX = 'ex';
 export const EXID = 'kfajbgpmhinphopgjjempdcgihajeejb';
